003e6cc464b050c9bdf7bae766969a78bbf1ac96
Muayyad1999 <37573322+Muayyad1999@users.noreply.github.com>
2021-09-30 19:52:41 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,44 +1,61 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+let fs = require('fs');
+let Input_Arr = fs.readFileSync(__dirname + '/\data.txt').toString().split(',');
 
-console.log("Original numbers list: ", numbers)
+const Numbers = [5,8,0,1,9,11,15,16];
 
-for(let i = 0 ; i < numbers.length ; i++)
+console.log("Original Numbers List: ", Numbers)
+
+bblSort_ASC(Numbers)
+console.log("Numbers List After ASC Sorting: ", Numbers)
+bblSort_DESC(Numbers)
+console.log("Numbers List After DESC Sorting: ", Numbers)
+
+console.log("Input Array List Before Sorting: ", Input_Arr)
+
+function bblSort_ASC(arr)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
+    for(let i = 0; i < arr.length; i++)
+    { 
+        for(let j = 0; j < (arr.length - i - 1); j++)
         {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            if(arr[j] > arr[j + 1])
+            {
+                let temp = arr[j];
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp;
+            }
         }
     }
 }
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
+function bblSort_DESC(arr)
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
+    for(let i = 0; i < arr.length; i++)
+    { 
+        for(let j = 0; j < (arr.length - i - 1); j++)
         {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            if(arr[j] < arr[j + 1])
+            {
+                let temp = arr[j];
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp;
+            }
         }
     }
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
+bblSort_ASC(Input_Arr);
+Save_Output()
+console.log("Input Array List After ASC Sorting: ", Input_Arr)
+bblSort_DESC(Input_Arr);
+console.log("Input Array List After DESC Sorting: ", Input_Arr)
 
 
+function Save_Output()
+{
+    let file = fs.createWriteStream(__dirname + '/\output.txt');
+    file.on('error', function(err) { Console.log(err) });
+    let str = Input_Arr.join(','); 
+    file.write(str);
+    file.end();
+}
