44c396106bd6415ba2c1cd324efd0a9661778652
Muayyad1999 <37573322+Muayyad1999@users.noreply.github.com>
2021-09-30 20:16:31 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,18 +1,18 @@
-let fs = require('fs');
-let Input_Arr = fs.readFileSync(__dirname + '/\data.txt').toString().split(',');
+let fs = require('fs'); // Please use Node.js
+let Input_Arr = fs.readFileSync(__dirname + '/\data.txt').toString().split(','); // Reading data.txt numbers from repo directory
 
-const Numbers = [5,8,0,1,9,11,15,16];
+const Numbers = [5,8,0,1,9,11,15,16]; 
 
 console.log("Original Numbers List: ", Numbers)
 
-bblSort_ASC(Numbers)
+bblSort_ASC(Numbers) // Calling Ascending Bubble Sort Function For Numbers Array
 console.log("Numbers List After ASC Sorting: ", Numbers)
-bblSort_DESC(Numbers)
+bblSort_DESC(Numbers) // Calling Descending Bubble Sort Function For Numbers Array
 console.log("Numbers List After DESC Sorting: ", Numbers)
 
 console.log("Input Array List Before Sorting: ", Input_Arr)
 
-function bblSort_ASC(arr)
+function bblSort_ASC(arr) // Bubble Sort Function For Ascending Sorting
 {
     for(let i = 0; i < arr.length; i++)
     { 
@@ -28,7 +28,7 @@ function bblSort_ASC(arr)
     }
 }
 
-function bblSort_DESC(arr)
+function bblSort_DESC(arr) // Bubble Sort Function For Descending Sorting
 {
     for(let i = 0; i < arr.length; i++)
     { 
@@ -44,14 +44,14 @@ function bblSort_DESC(arr)
     }
 }
 
-bblSort_ASC(Input_Arr);
+bblSort_ASC(Input_Arr); // Calling Bubble Ascending Sort Function For Input Array From data.txt
 Save_Output()
 console.log("Input Array List After ASC Sorting: ", Input_Arr)
-bblSort_DESC(Input_Arr);
+bblSort_DESC(Input_Arr); // Calling Bubble Descending Sort Function For Input Array From data.txt
 console.log("Input Array List After DESC Sorting: ", Input_Arr)
 
 
-function Save_Output()
+function Save_Output() // Saving Result In output.txt For The Ascending Bubble Sort Of The Input Array From data.txt As Requested In To-Do's
 {
     let file = fs.createWriteStream(__dirname + '/\output.txt');
     file.on('error', function(err) { Console.log(err) });
